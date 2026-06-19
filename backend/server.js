72c26ec0ef2fc7860f18b4731937fa5e87b91856
sahul-yadav <yadavsahul220@gmail.com>
2026-06-19 12:36:34 +0530

    Fix CORS origin URL

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,29 @@
-require("dotenv").config()
-const express = require("express")
-const connectDB =require("./src/db/connectdb")
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./src/db/connectdb");
+const cors = require("cors");
+
 const app = express();
-const contactRouter= require("./src/router/contact")
-const cors = require("cors")
 
+app.use(
+  cors({
+    origin: "https://workshop-landing-page-s2uw.vercel.app"
+  })
+);
 
+app.use(express.json());
 
-
-app.use(cors({
-  origin: "https://workshop-landing-page-s2uw.vercel.app/"
-}));
-
-app.use(express.json())
-app.use("/contact", contactRouter)
+const contactRouter = require("./src/router/contact");
+app.use("/contact", contactRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello TypeScript + Express!");
 });
 
-connectDB()
+connectDB();
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${PORT}`);
 });
